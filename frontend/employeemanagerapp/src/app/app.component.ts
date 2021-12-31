02727b104e9a98b1fb191ded34e8d25b953d3a4e
aschlichter <adipostaja@gmail.com>
2021-12-31 08:46:05 +0100

    UI modal added.

--- frontend/employeemanagerapp/src/app/app.component.ts
+++ frontend/employeemanagerapp/src/app/app.component.ts
@@ -8,14 +8,14 @@ import { EmployeeService } from './employee.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'employeemanagerapp';
   public employees!: Employee[];
 
-  constructor(private employeeService: EmployeeService) {}
+  constructor(private employeeService: EmployeeService) { }
 
   ngOnInit(): void {
-      this.getEmployees();
+    this.getEmployees();
   }
 
   public getEmployees(): void {
@@ -26,6 +26,26 @@ export class AppComponent implements OnInit{
       (error: HttpErrorResponse) => {
         alert(error.message);
       }
-    )
+    );
   }
+
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container')
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal')
+    }
+    if (mode === 'edit') {
+      button.setAttribute('data-target', '#updateEmployeeModal')
+    }
+    if (mode === 'delete') {
+      button.setAttribute('data-target', '#deleteEmployeeModal')
+    }
+    container!.appendChild(button);
+    button.click();
+  }
+
 }
